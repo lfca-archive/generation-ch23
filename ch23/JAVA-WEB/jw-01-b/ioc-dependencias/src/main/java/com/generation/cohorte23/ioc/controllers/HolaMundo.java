a6ed481ab3a2cf27b223a9550ba0e619c0cfc1ad
Luisa <luiisa.castillo21@gmail.com>
2023-03-24 18:57:20 -0600

    ejercicios jw, se hizo crud

---
+++ ch23/JAVA-WEB/jw-01-b/ioc-dependencias/src/main/java/com/generation/cohorte23/ioc/controllers/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.generation.cohorte23.ioc.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundo {
+	@RequestMapping("/hola")
+	public String hola() {
+		return "Hola mundo desde spring boot";
+	}
+
+}
---
+++ ch23/JAVA-WEB/jw-01-b/orm.jpa/src/main/java/com/generation/cohorte23/jpa/services/UsuarioServices.java
@@ -0,0 +1,46 @@
+package com.generation.cohorte23.jpa.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import com.generation.cohorte23.jpa.models.UsuarioModel;
+import com.generation.cohorte23.jpa.repositories.UsuarioRepositorio;
+
+@Service
+public class UsuarioServices {
+	@Autowired
+	UsuarioRepositorio usuariorepositorio;
+	@DeleteMapping
+	public boolean eliminarUsuario(Long id) {
+		try {
+			usuariorepositorio.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+		
+		
+
+	public ArrayList<UsuarioModel> ListarUsuarios() {
+		return (ArrayList<UsuarioModel>) usuariorepositorio.findAll();
+
+	}
+	
+	public UsuarioModel guardarUsuario(UsuarioModel usuario) {
+		return usuariorepositorio.save(usuario);
+	}
+	
+	public ArrayList<UsuarioModel> obtenerPorPrioridad(Integer prioridad) {
+		return usuariorepositorio.findByPrioridad(prioridad);
+	}
+
+	public Optional<UsuarioModel> obtenerPorId(Long id){
+		return usuariorepositorio.findById(id);
+	}
+	
+}
